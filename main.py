fa637ec31ea5d61c54ef59d55faf247605193d81
Lenovo <cs.saif141@gmail.com>
2022-07-08 21:28:08 +0300

    Adding two numbers inside a text value

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import List
+def addStrNums(num1: str, num2: str) -> str:
+    try:
+        a=int(num1)
+        b=int(num2)
+        sum=a+b
+        sumf=str(sum)
+        return sumf
+    except:
+        return "-1"
